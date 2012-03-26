6a40937b27906a88c88080fe788cf06119835897
Mark Morgan <momipsl@ipsl.jussieu.fr>
2012-03-26 18:29:28 +0200

    Extended activity package in order to support simulations

---
+++ pycim/v1_5/types/activity/physical_modification.py
@@ -0,0 +1,78 @@
+"""A class within cim v1.5 type system.
+
+CIM CODE GENERATOR :: Code generated @ 2012-03-26 18:08:48.732857.
+"""
+
+# Module imports.
+import datetime
+import simplejson
+import types
+import uuid
+
+# Intra/Inter-package imports.
+from pycim.v1_5.types.activity.conformance import Conformance
+
+
+
+# Module exports.
+__all__ = ['PhysicalModification']
+
+
+# Module provenance info.
+__author__="Mark Morgan"
+__copyright__ = "Copyright 2012 - Institut Pierre Simon Laplace."
+__date__ ="$2012-03-26 18:08:48.732857$"
+__license__ = "GPL"
+__version__ = "1.5.0"
+__maintainer__ = "Mark Morgan"
+__email__ = "momipsl@ipsl.jussieu.fr"
+__status__ = "Production"
+
+
+
+class PhysicalModification(Conformance):
+    """A class within the cim v1.5 type system.
+
+    Physical modification is the implementation of a boundary condition numerical requirement that is achieved within the model code rather than from some external source file. It  might include, for example,  a specific rate constant within a chemical reaction, or coefficient value(s) in a parameterisation.  For example, one might require a numerical experiment where specific chemical reactions were turned off - e.g. no heterogeneous chemistry.
+    """
+    def __init__(self):
+        """Constructor"""
+        super(PhysicalModification, self).__init__()
+
+        pass
+
+
+    def as_dict(self):
+        """Gets dictionary representation of self used to create other representations such as json, xml ...etc.
+
+        """
+        def append(d, key, value, is_iterative, is_primitive, is_enum):
+            if value is None:
+                if is_iterative:
+                    value = []
+            elif is_primitive == False and is_enum == False:
+                if is_iterative:
+                    value = map(lambda i : i.as_dict(), value)
+                else:
+                    value = value.as_dict()
+            d[key] = value
+
+        # Populate a deep dictionary.
+        d = dict(super(PhysicalModification, self).as_dict())
+        return d
+
+
+
+
+
+# Circular reference imports.
+# N.B. - see http://effbot.org/zone/import-confusion.htm.
+
+
+
+# Command line entry point.
+if __name__ == "__main__":
+    try:
+        i = PhysicalModification()
+    except Exception as e:
+        print "CIM INSTANTIATION EXCEPTION !!! :: TYPE = {0} : ERROR = {1}.".format('activity.PhysicalModification', e)
---
+++ pycim/v1_5/types/shared/standard.py
@@ -0,0 +1,145 @@
+"""A class within cim v1.5 type system.
+
+CIM CODE GENERATOR :: Code generated @ 2012-03-26 18:08:48.800136.
+"""
+
+# Module imports.
+import datetime
+import simplejson
+import types
+import uuid
+
+# Intra/Inter-package imports.
+
+
+
+# Module exports.
+__all__ = ['Standard']
+
+
+# Module provenance info.
+__author__="Mark Morgan"
+__copyright__ = "Copyright 2012 - Institut Pierre Simon Laplace."
+__date__ ="$2012-03-26 18:08:48.800136$"
+__license__ = "GPL"
+__version__ = "1.5.0"
+__maintainer__ = "Mark Morgan"
+__email__ = "momipsl@ipsl.jussieu.fr"
+__status__ = "Production"
+
+
+
+class Standard(object):
+    """A class within the cim v1.5 type system.
+
+    Describes a name given to an entity from a recognised standard.  The CIM records the standard and the name.  For example, the standard might be CF and the name might be atmospheric_pressure.
+    """
+    def __init__(self):
+        """Constructor"""
+        super(Standard, self).__init__()
+
+        self.__description = str()                                  # type = str
+        self.__name = str()                                         # type = str
+        self.__version = str()                                      # type = str
+
+
+    @property
+    def description(self):
+        """Gets value of {class-name} description property.
+
+        The version of the standard"""
+        return self.__description
+
+    @description.setter
+    def description(self, value):
+        if value is not None and not isinstance(value, str):
+            raise TypeError("Invalid value type  : VALUE = {0}.".format(value))
+        """Sets value of {class-name} description property."""
+        self.__description = value
+
+    @description.deleter
+    def description(self, value):
+        """Deletes {class-name} description property."""
+        raise TypeError("Cannot delete {class-name} description property.")
+
+
+    @property
+    def name(self):
+        """Gets value of standard name property.
+
+        The name of the standard"""
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value is None:
+            raise TypeError("Value cannot be null.")
+        elif not isinstance(value, str):
+            raise TypeError("Invalid value type : VALUE = {0}.".format(value))
+        """Sets value of standard name property."""
+        self.__name = value
+
+    @name.deleter
+    def name(self, value):
+        """Deletes standard name property."""
+        raise TypeError("Cannot delete standard name property.")
+
+
+    @property
+    def version(self):
+        """Gets value of {class-name} version property.
+
+        The version of the standard"""
+        return self.__version
+
+    @version.setter
+    def version(self, value):
+        if value is not None and not isinstance(value, str):
+            raise TypeError("Invalid value type  : VALUE = {0}.".format(value))
+        """Sets value of {class-name} version property."""
+        self.__version = value
+
+    @version.deleter
+    def version(self, value):
+        """Deletes {class-name} version property."""
+        raise TypeError("Cannot delete {class-name} version property.")
+
+
+
+    def as_dict(self):
+        """Gets dictionary representation of self used to create other representations such as json, xml ...etc.
+
+        """
+        def append(d, key, value, is_iterative, is_primitive, is_enum):
+            if value is None:
+                if is_iterative:
+                    value = []
+            elif is_primitive == False and is_enum == False:
+                if is_iterative:
+                    value = map(lambda i : i.as_dict(), value)
+                else:
+                    value = value.as_dict()
+            d[key] = value
+
+        # Populate a deep dictionary.
+        d = dict()
+        append(d, 'description', self.__description, False, True, False)
+        append(d, 'name', self.__name, False, True, False)
+        append(d, 'version', self.__version, False, True, False)
+        return d
+
+
+
+
+
+# Circular reference imports.
+# N.B. - see http://effbot.org/zone/import-confusion.htm.
+
+
+
+# Command line entry point.
+if __name__ == "__main__":
+    try:
+        i = Standard()
+    except Exception as e:
+        print "CIM INSTANTIATION EXCEPTION !!! :: TYPE = {0} : ERROR = {1}.".format('shared.Standard', e)
